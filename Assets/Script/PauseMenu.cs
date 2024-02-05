306aafe5ea1213a1cbfd0b45cd8daee20d632464
Sebastien H <sebic1708@gmail.com>
2024-02-05 15:58:52 +0100

    Logging
    +Logging des scripts

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -13,8 +13,10 @@ public class PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown("escape"))
         {
+            Debug.Log("PauseMenu: " + "Escape [IsPressed]");
             if (!activeEscape)
             {
+                
                 pauseMenu.SetActive(true);
                 activeEscape = true;
             }
@@ -28,27 +30,38 @@ public class PauseMenu : MonoBehaviour
     }
     public void Home()
     {
+        Debug.Log("PauseMenu: " + "Home [OK]");
         SceneManager.LoadScene(0);
     }
 
     public void Pause()
     {
+        Debug.Log("PauseMenu: " + "Pause [OK]");
         pauseMenu.SetActive(true);
     }
 
     public void Resume()
-    {
+    {   
+        Debug.Log("PauseMenu: " + "Resume [OK]");
         pauseMenu.SetActive(false);
         activeEscape = false;
     }
 
     public void Restart()
     {
+        Debug.Log("PauseMenu: " + "Restart [OK]");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void Parameters()
     {
+        Debug.Log("PauseMenu: " + "Parameters [OK]");
         settingsMenu.SetActive(true);
     }
+
+    public void QuitGame()
+   {
+    Debug.Log("PauseMenu: " + "QuitGame [OK]");
+    Application.Quit();
+   }
 }
