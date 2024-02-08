0f1ec01f0c104385aec8f98154789997fed2c23f
Sebastien H <sebic1708@gmail.com>
2024-02-08 14:40:27 +0100

    Health
    + Script heal
    + Script take domage

---
+++ Assets/Script/Health.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            //Animation of death
+            //Death screen
+        }
+    }
+
+    public void Heal(int heal)
+    {
+        currentHealth += heal;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+   
+}
